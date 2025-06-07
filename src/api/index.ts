578cc74f1f8e55a3a1346e8b5d50927ea3a2455d
TheComputerM <mudit.somani00@gmail.com>
2025-06-07 13:06:39 +0530

    feat: generate api documentation using swagger

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,16 @@
+import { auth } from "@/lib/auth";
+import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+	.use(
+		// visit `/swagger` to view generated documentation
+		swagger(),
+	)
+	.mount(auth.handler)
+	.get("/", () => "Hello Elysia");
+
+app.listen(3000);
 
 console.log(
 	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
