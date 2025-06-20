4ee1f30ca85668abc88b7f285d3bca426e2691ea
TheComputerM <mudit.somani00@gmail.com>
2025-06-20 14:10:23 +0530

    refactor: check if admin exists using inbuilt funcs

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -1,9 +1,9 @@
+import { and, eq } from "drizzle-orm";
+import { migrate } from "drizzle-orm/bun-sql/migrator";
 import { db } from "@/db";
 import { user } from "@/db/schema/auth";
 import { auth } from "@/lib/auth";
 import env from "@/lib/env";
-import { sql } from "drizzle-orm";
-import { migrate } from "drizzle-orm/bun-sql/migrator";
 
 console.log("Running Drizzle SQL migrations...");
 if (await Bun.file("./drizzle/meta/_journal.json").exists()) {
@@ -16,11 +16,12 @@ if (await Bun.file("./drizzle/meta/_journal.json").exists()) {
 }
 
 console.log("Checking if Admin user exists...");
-const [{ admin_exists }] = await db.execute<{ admin_exists: boolean }>(
-	sql`SELECT EXISTS (SELECT 1 FROM ${user} WHERE ${user.role} = ${"admin"} AND ${user.email} = ${env.ADMIN_EMAIL}) as admin_exists`,
+const adminCount = await db.$count(
+	user,
+	and(eq(user.email, env.ADMIN_EMAIL), eq(user.role, "admin")),
 );
 
-if (!admin_exists) {
+if (adminCount === 0) {
 	console.log("Creating Admin user...");
 	await auth.api.createUser({
 		body: {
