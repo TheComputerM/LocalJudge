3890b4047110565130ad29ce7cc93c37d9450bfe
TheComputerM <mudit.somani00@gmail.com>
2025-07-03 01:27:37 +0530

    refactor: remove unnecessary packages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { LucideLogIn } from "lucide-react";
-import { TypingAnimation } from "@/components/magicui/typing-animation";
 import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/")({
@@ -11,9 +10,7 @@ function Home() {
 	return (
 		<div>
 			<div className="text-center py-16 px-4 space-y-6">
-				<TypingAnimation className="font-bold text-6xl" as="h1">
-					LocalJudge
-				</TypingAnimation>
+				<h1 className="font-bold text-6xl">LocalJudge</h1>
 				<p className="text-xl text-balance">
 					Open-source coding judge. Easy to host. Easy to run. Fully under your
 					control.
