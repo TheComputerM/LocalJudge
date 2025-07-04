923d90b8072dc7414b265c1dec0013a4c75c3e4a
TheComputerM <mudit.somani00@gmail.com>
2025-07-04 19:05:54 +0530

    refactor: move contest admin to diff file

--- src/api/admin/index.ts
+++ src/api/admin/index.ts
@@ -1,110 +1,9 @@
-import { and, asc, desc, eq } from "drizzle-orm";
-import Elysia, { status, t } from "elysia";
+import { eq } from "drizzle-orm";
+import Elysia from "elysia";
 import { betterAuthPlugin } from "@/api/better-auth";
 import { db } from "@/db";
 import * as table from "@/db/schema";
-import { ContestModel } from "@/db/typebox/contest";
-
-const contestController = new Elysia()
-	.get(
-		"/",
-		async () =>
-			await db
-				.select()
-				.from(table.contest)
-				.orderBy(desc(table.contest.startTime)),
-		{
-			detail: {
-				summary: "Get contests",
-				description: "Get all contests present in the system",
-			},
-		},
-	)
-	.post(
-		"/",
-		async ({ body }) => {
-			const [data] = await db.insert(table.contest).values(body).returning();
-			return data;
-		},
-		{
-			body: ContestModel.insert,
-			detail: {
-				summary: "Create new contest",
-				description: "Create a new contest with the given details",
-			},
-		},
-	)
-	.group("/:id", (app) =>
-		app
-			.get(
-				"/",
-				async ({ params }) => {
-					const contest = await db.query.contest.findFirst({
-						where: eq(table.contest.id, params.id),
-					});
-					if (!contest) return status(404, "Contest not found");
-					return contest;
-				},
-				{
-					detail: {
-						summary: "Get contest",
-						description: "Get details of a specific contest by its ID",
-					},
-				},
-			)
-			.group("/problem", (app) =>
-				app
-					.get(
-						"/",
-						async ({ params }) => {
-							const problems = db.query.problem.findMany({
-								where: eq(table.problem.contestId, params.id),
-								columns: {
-									description: false,
-								},
-								orderBy: asc(table.problem.number),
-							});
-							return problems;
-						},
-						{
-							detail: {
-								summary: "Get contest problems",
-								description: "Get all problems of a specific contest by its ID",
-							},
-						},
-					)
-					.group(
-						"/:problem",
-						{
-							params: t.Object({
-								id: t.String(),
-								problem: t.Number(),
-							}),
-						},
-						(app) =>
-							app.get(
-								"/",
-								async ({ params }) => {
-									const problem = await db.query.problem.findFirst({
-										where: and(
-											eq(table.problem.contestId, params.id),
-											eq(table.problem.number, params.problem),
-										),
-									});
-									if (!problem) return status(404, "Problem not found");
-									return problem;
-								},
-								{
-									detail: {
-										summary: "Get contest problem",
-										description:
-											"Get details of a specific problem in a contest by its ID and problem number",
-									},
-								},
-							),
-					),
-			),
-	);
+import { adminContestApp } from "./contest";
 
 export const adminApp = new Elysia({
 	prefix: "/admin",
@@ -137,4 +36,4 @@ export const adminApp = new Elysia({
 			},
 		},
 	)
-	.group("/contest", (app) => app.use(contestController));
+	.group("/contest", (app) => app.use(adminContestApp));
