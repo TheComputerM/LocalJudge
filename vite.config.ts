d2ab7ac6a7ff44b2ce2db95d5e6f41facc6b95f1
TheComputerM <mudit.somani00@gmail.com>
2025-07-11 21:12:46 +0530

    chore: update tanstack start

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
@@ -12,6 +13,8 @@ export default defineConfig({
 		tailwindcss(),
 		tanstackStart({
 			target: "bun",
+			customViteReactPlugin: true,
 		}),
+		react(),
 	],
 });
