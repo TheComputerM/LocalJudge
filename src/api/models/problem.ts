e8f4c745cd19948a2f105e6602be80fbdcae9872
TheComputerM <mudit.somani00@gmail.com>
2025-08-11 08:45:27 +0530

    feat: add update schema for problem model

--- src/api/models/problem.ts
+++ src/api/models/problem.ts
@@ -1,6 +1,6 @@
 import { t } from "elysia";
 import { problem } from "@/db/schema";
-import { createInsertSchema, createSelectSchema } from ".";
+import { createInsertSchema, createSelectSchema, createUpdateSchema } from ".";
 
 const timeLimit = t.Number({
 	title: "Time Limit",
@@ -22,9 +22,15 @@ const _insertSchema = createInsertSchema(problem, {
 	timeLimit: t.Optional(timeLimit),
 	memoryLimit: t.Optional(memoryLimit),
 });
+const _updateSchema = createUpdateSchema(problem, {
+	timeLimit: t.Optional(timeLimit),
+	memoryLimit: t.Optional(memoryLimit),
+});
 
 export namespace ProblemModel {
 	export const select = t.Omit(_selectSchema, ["contestId"]);
 	export const groupSelect = t.Array(t.Omit(select, ["description"]));
+
 	export const insert = t.Omit(_insertSchema, ["contestId", "number"]);
+	export const update = t.Omit(_updateSchema, ["contestId", "number"]);
 }
