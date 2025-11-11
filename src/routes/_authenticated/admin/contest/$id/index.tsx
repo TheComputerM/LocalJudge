81e9d284b352c8a8d8bfddb1ec369ba6283729d7
TheComputerM <mudit.somani00@gmail.com>
2025-11-12 02:42:27 +0530

    feat: add copy contest code button

--- src/routes/_authenticated/admin/contest/$id/index.tsx
+++ src/routes/_authenticated/admin/contest/$id/index.tsx
@@ -1,5 +1,8 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
+import { formatDistanceToNow } from "date-fns";
 import {
+	LucideCheck,
+	LucideCopy,
 	LucideEye,
 	LucideFileBadge,
 	LucideFolderArchive,
@@ -8,6 +11,7 @@ import {
 } from "lucide-react";
 import { localjudge } from "@/api/client";
 import { ConfirmActionDialog } from "@/components/confirm-action";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
 	Card,
@@ -26,6 +30,7 @@ import {
 } from "@/components/ui/item";
 import { Separator } from "@/components/ui/separator";
 import { toastManager } from "@/components/ui/toast";
+import { useCopyToClipboard } from "@/hooks/use-copy-to-clipboard";
 import { rejectError } from "@/lib/utils";
 
 export const Route = createFileRoute("/_authenticated/admin/contest/$id/")({
@@ -33,7 +38,7 @@ export const Route = createFileRoute("/_authenticated/admin/contest/$id/")({
 		const overview = await rejectError(
 			localjudge.admin.contest({ id: params.id }).overview.get(),
 		);
-		return { overview };
+		return overview;
 	},
 	component: RouteComponent,
 });
@@ -164,24 +169,68 @@ function ContestLeaderboard() {
 	);
 }
 
+function Overview() {
+	const overview = Route.useLoaderData();
+	const contest = Route.useRouteContext({ select: (ctx) => ctx.contest });
+	return (
+		<div className="grid sm:grid-cols-2 md:grid-cols-6 gap-3">
+			<Card className="md:col-span-3">
+				<CardHeader>
+					<CardDescription>Start Time</CardDescription>
+					<CardTitle>{contest.startTime.toLocaleString()}</CardTitle>
+				</CardHeader>
+			</Card>
+			<Card className="md:col-span-3">
+				<CardHeader>
+					<CardDescription>End Time</CardDescription>
+					<CardTitle>{contest.endTime.toLocaleString()}</CardTitle>
+				</CardHeader>
+			</Card>
+			{Object.entries(overview).map(([name, value]) => (
+				<Card key={name} className="md:col-span-2">
+					<CardHeader>
+						<CardDescription className="capitalize">{name}</CardDescription>
+						<CardTitle className="text-2xl font-semibold tabular-nums">
+							{value}
+						</CardTitle>
+					</CardHeader>
+				</Card>
+			))}
+		</div>
+	);
+}
+
+function ContestId() {
+	const id = Route.useParams({ select: ({ id }) => id });
+	const [copy, isCoped] = useCopyToClipboard();
+	return (
+		<Item>
+			<ItemContent>
+				<ItemTitle>Copy Contest Code</ItemTitle>
+				<ItemDescription>
+					This is the contest ID which is same as the code used to register for
+					a contest.
+				</ItemDescription>
+			</ItemContent>
+			<ItemActions>
+				<Button onClick={() => copy(id)}>
+					{isCoped ? <LucideCheck /> : <LucideCopy />}
+					Copy ID
+				</Button>
+			</ItemActions>
+		</Item>
+	);
+}
+
 function RouteComponent() {
-	const overview = Route.useLoaderData({ select: ({ overview }) => overview });
 	return (
 		<div>
-			<div className="grid sm:grid-cols-2 md:grid-cols-3 gap-3">
-				{Object.entries(overview).map(([name, value]) => (
-					<Card key={name}>
-						<CardHeader>
-							<CardDescription className="capitalize">{name}</CardDescription>
-							<CardTitle className="text-2xl font-semibold tabular-nums">
-								{value}
-							</CardTitle>
-						</CardHeader>
-					</Card>
-				))}
-			</div>
+			<Overview />
 			<Separator className="my-6" />
 			<ItemGroup>
+				{/*TODO: maybe add heading*/}
+				<ContestId />
+				<ItemSeparator />
 				<InspectContest />
 				<ItemSeparator />
 				<ContestLeaderboard />
